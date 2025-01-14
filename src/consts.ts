b7d03c41647014304b35bbe60add06daf4a49e80
DanielAgg <daniel.agg@outlook.com>
2025-01-14 21:40:24 +0100

    reduce site desc

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
 export const SITE_TITLE = "Daniel Agg";
 export const SITE_DESCRIPTION =
-  "Hi! I'm a senior full stack developer, located in Amsterdam. This is personal website to showcase my public projects & blog.";
+  "Hi! I'm a senior full stack developer, based in Amsterdam.";
