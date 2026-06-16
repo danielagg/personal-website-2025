e3cf0237da4cf368ef6d4b6032acab4b598c19e9
Daniel Agg <danielagg@ipractice.nl>
2026-06-16 11:35:29 +0200

    feat: upgrade packages, switch to bun

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
-import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    integrations: [mdx(), sitemap(), tailwind()],
+    integrations: [mdx(), sitemap()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
